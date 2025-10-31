53ee3ab5c3f60efc2ab74cec3a4f53c556fccd29
khaiamgaraevv <khaiamgaraevv>
2025-10-31 22:01:28 +0400

    update: yeni fayllar və dəyişikliklər

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
 func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.SendString("Salam, Dünya! 👋")
 	})
+
+	app.Get("/about", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"author": "Xəyyam",
+			"app":    "Go Fiber Project",
+		})
+	})
+
+	app.Listen(":3000")
 }
